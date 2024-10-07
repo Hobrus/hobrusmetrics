ca0eb40edc72e267391ea6d65f4ce13cf1699336
Hobrus <bhobrus@gmail.com>
2024-10-07 20:16:55 +0300

    refactor: simplify handlers.go

--- internal/app/server/handlers/handlers.go
+++ internal/app/server/handlers/handlers.go
@@ -17,68 +17,62 @@ func NewHandler(ms *service.MetricsService) *Handler {
 }
 
 func (h *Handler) SetupRoutes(router *gin.Engine) {
-	router.POST("/update/:type/:name/:value", h.updateHandler())
-	router.GET("/value/:type/:name", h.getValueHandler())
-	router.GET("/", h.getAllMetricsHandler())
+	router.POST("/update/:type/:name/:value", h.updateHandler)
+	router.GET("/value/:type/:name", h.getValueHandler)
+	router.GET("/", h.getAllMetricsHandler)
 }
 
-func (h *Handler) updateHandler() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		metricType := c.Param("type")
-		metricName := c.Param("name")
-		metricValue := c.Param("value")
+func (h *Handler) updateHandler(c *gin.Context) {
+	metricType := c.Param("type")
+	metricName := c.Param("name")
+	metricValue := c.Param("value")
 
-		err := h.ms.UpdateMetric(metricType, metricName, metricValue)
-		if err != nil {
-			c.String(http.StatusBadRequest, err.Error())
-			return
-		}
-
-		c.Status(http.StatusOK)
+	err := h.ms.UpdateMetric(metricType, metricName, metricValue)
+	if err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+		return
 	}
+
+	c.Status(http.StatusOK)
 }
 
-func (h *Handler) getValueHandler() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		metricType := c.Param("type")
-		metricName := c.Param("name")
+func (h *Handler) getValueHandler(c *gin.Context) {
+	metricType := c.Param("type")
+	metricName := c.Param("name")
 
-		value, err := h.ms.GetMetricValue(metricType, metricName)
-		if err != nil {
-			c.Status(http.StatusNotFound)
-			return
-		}
-
-		c.String(http.StatusOK, value)
+	value, err := h.ms.GetMetricValue(metricType, metricName)
+	if err != nil {
+		c.Status(http.StatusNotFound)
+		return
 	}
+
+	c.String(http.StatusOK, value)
 }
 
-func (h *Handler) getAllMetricsHandler() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		metrics := h.ms.GetAllMetrics()
+func (h *Handler) getAllMetricsHandler(c *gin.Context) {
+	metrics := h.ms.GetAllMetrics()
 
-		tmpl, err := template.New("metrics").Parse(`
-		<html>
-			<body>
-				<h1>Metrics</h1>
-				<ul>
-					{{range $name, $value := .}}
-						<li>{{$name}}: {{$value}}</li>
-					{{end}}
-				</ul>
-			</body>
-		</html>
-		`)
-		if err != nil {
-			c.String(http.StatusInternalServerError, "Error rendering template")
-			return
-		}
+	tmpl, err := template.New("metrics").Parse(`
+    <html>
+        <body>
+            <h1>Metrics</h1>
+            <ul>
+                {{range $name, $value := .}}
+                    <li>{{$name}}: {{$value}}</li>
+                {{end}}
+            </ul>
+        </body>
+    </html>
+    `)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Error rendering template")
+		return
+	}
 
-		c.Header("Content-Type", "text/html")
-		err = tmpl.Execute(c.Writer, metrics)
-		if err != nil {
-			c.String(http.StatusInternalServerError, "Error rendering template")
-			return
-		}
+	c.Header("Content-Type", "text/html")
+	err = tmpl.Execute(c.Writer, metrics)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Error rendering template")
+		return
 	}
 }
