59cfab451b76dade4dd45300ce672d2f3c84a111
Hobrus <bhobrus@gmail.com>
2024-10-07 19:52:02 +0300

    refactor!: refactored storage.go

--- internal/app/server/repository/storage.go
+++ internal/app/server/repository/storage.go
@@ -16,61 +16,80 @@ type Storage interface {
 	GetAllCounters() map[string]Counter
 }
 
+type Numeric interface {
+	~int64 | ~float64
+}
+
+type MetricStorage[T Numeric] struct {
+	mu     sync.RWMutex
+	values map[string]T
+}
+
+func NewMetricStorage[T Numeric]() *MetricStorage[T] {
+	return &MetricStorage[T]{
+		values: make(map[string]T),
+	}
+}
+
+func (m *MetricStorage[T]) Update(name string, value T, accumulate bool) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if accumulate {
+		m.values[name] += value
+	} else {
+		m.values[name] = value
+	}
+}
+
+func (m *MetricStorage[T]) Get(name string) (T, bool) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	value, exists := m.values[name]
+	return value, exists
+}
+
+func (m *MetricStorage[T]) GetAll() map[string]T {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	valuesCopy := make(map[string]T, len(m.values))
+	for k, v := range m.values {
+		valuesCopy[k] = v
+	}
+	return valuesCopy
+}
+
 type MemStorage struct {
-	mu       sync.RWMutex
-	gauges   map[string]Gauge
-	counters map[string]Counter
+	gauges   *MetricStorage[Gauge]
+	counters *MetricStorage[Counter]
 }
 
 func NewMemStorage() *MemStorage {
 	return &MemStorage{
-		gauges:   make(map[string]Gauge),
-		counters: make(map[string]Counter),
+		gauges:   NewMetricStorage[Gauge](),
+		counters: NewMetricStorage[Counter](),
 	}
 }
 
 func (m *MemStorage) UpdateGauge(name string, value Gauge) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	m.gauges[name] = value
+	m.gauges.Update(name, value, false)
 }
 
 func (m *MemStorage) UpdateCounter(name string, value Counter) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	m.counters[name] += value
+	m.counters.Update(name, value, true)
 }
 
 func (m *MemStorage) GetGauge(name string) (Gauge, bool) {
-	m.mu.RLock()
-	defer m.mu.RUnlock()
-	value, exists := m.gauges[name]
-	return value, exists
+	return m.gauges.Get(name)
 }
 
 func (m *MemStorage) GetCounter(name string) (Counter, bool) {
-	m.mu.RLock()
-	defer m.mu.RUnlock()
-	value, exists := m.counters[name]
-	return value, exists
+	return m.counters.Get(name)
 }
 
 func (m *MemStorage) GetAllGauges() map[string]Gauge {
-	m.mu.RLock()
-	defer m.mu.RUnlock()
-	gauges := make(map[string]Gauge)
-	for k, v := range m.gauges {
-		gauges[k] = v
-	}
-	return gauges
+	return m.gauges.GetAll()
 }
 
 func (m *MemStorage) GetAllCounters() map[string]Counter {
-	m.mu.RLock()
-	defer m.mu.RUnlock()
-	counters := make(map[string]Counter)
-	for k, v := range m.counters {
-		counters[k] = v
-	}
-	return counters
+	return m.counters.GetAll()
 }
