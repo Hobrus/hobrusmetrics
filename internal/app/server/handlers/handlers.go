7c2aee438de6419d2fadc400d81eeaad7528cd83
Hobrus <bhobrus@gmail.com>
2024-10-15 03:15:13 +0300

    refactor: try another template 4

--- internal/app/server/handlers/handlers.go
+++ internal/app/server/handlers/handlers.go
@@ -9,7 +9,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-//go:embed web/templates/*.html
+//go:embed template/*.html
 var templatesFS embed.FS
 
 type Handler struct {
@@ -57,7 +57,7 @@ func (h *Handler) getAllMetricsHandler(c *gin.Context) {
 	metrics := h.ms.GetAllMetrics()
 
 	// Parse the embedded template
-	tmpl, err := template.ParseFS(templatesFS, "web/template/metrics.html")
+	tmpl, err := template.ParseFS(templatesFS, "template/metrics.html")
 	if err != nil {
 		c.String(http.StatusInternalServerError, "Error rendering template")
 		return
